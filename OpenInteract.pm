e627849597a8dcd55551a388b33cdbbc8c64f04b
Chris Winters <chris@cwinters.com>
2003-04-13 16:48:10 -0800

    import OpenInteract 1.58 from CPAN
    git-cpan-module:   OpenInteract
    git-cpan-version:  1.58
    git-cpan-authorid: CWINTERS
    git-cpan-file:     authors/id/C/CW/CWINTERS/OpenInteract-1.58.tar.gz

--- OpenInteract.pm
+++ OpenInteract.pm
@@ -7,7 +7,7 @@ use Apache::Constants qw( :common :remotehost );
 use Apache::Request;
 use Data::Dumper      qw( Dumper );
 
-$OpenInteract::VERSION  = '1.57';
+$OpenInteract::VERSION  = '1.58';
 
 # Generic separator used in display
 
