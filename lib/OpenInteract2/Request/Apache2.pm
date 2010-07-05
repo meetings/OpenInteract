36437df16448e105e52d2280aed4be5bc17a0dd9
root <root@dcp-dev.(none)>
2010-07-05 16:22:59 +0300

    Parse params with Apache2::Request

--- lib/OpenInteract2/Request/Apache2.pm
+++ lib/OpenInteract2/Request/Apache2.pm
@@ -10,6 +10,9 @@ use OpenInteract2::Context   qw( CTX );
 use OpenInteract2::Exception qw( oi_error );
 use OpenInteract2::Upload;
 
+use Apache2::Request;
+use Apache2::Upload;
+
 $OpenInteract2::Request::Apache2::VERSION = sprintf("%d.%02d", q$Revision: 1.5 $ =~ /(\d+)\.(\d+)/);
 
 my ( $log );
@@ -31,8 +34,10 @@ sub init {
         oi_error "Cannot initialize the OpenInteract2::Request object - ",
                  "pass in an Apache2 request object in 'apache'";
     }
+    
+    my $apache = Apache2::Request->new($r);
 
-    $self->apache( $r );
+    $self->apache( $apache );
 
     # Hopefully this will pull the right Apache::Request...
     unless ( $done ) {
@@ -52,10 +57,29 @@ sub init {
     $log->is_debug && $log->debug( "Got URL from apache2 '$full_url'" );
     $self->assign_request_url( $full_url );
 
-    # HACK!
-    my $cgi = CGI->new( $r );
-    require OpenInteract2::Request::CGI;
-    OpenInteract2::Request::CGI::_assign_params_from_cgi( $self, $cgi );
+    # Setup the GET/POST params
+    
+    for my $field ($self->apache->param) {
+        my @values = $self->apache->param($field);
+
+        $self->param($field => (@values > 1 ? \@values : $values[0]));
+    }
+
+    # Uploads
+    
+    for my $upload_name ($self->apache->upload) {
+        my $upload = $self->apache->upload($upload_name);
+
+        my $oi_upload = OpenInteract2::Upload->new({
+            name         => $upload->name,
+            content_type => $upload->type,
+            size         => $upload->size,
+            filehandle   => $upload->fh,
+            filename     => $upload->filename,
+            tmp_name     => $upload->tempname
+        });
+        $self->_set_upload( $upload->name => $oi_upload );
+    }
 
     # Then the various headers, properties, etc.
 
@@ -73,83 +97,6 @@ sub init {
     $log->is_info &&
         $log->info( "Finished creating Apache 2.x request" );
 
-    return $self;
-
-    ##################################################
-
-    # TODO - set parameters here like:
-    #   TEMP_DIR => $WEBSITE_DIR/tmp
-
-    my $apache = Apache::Request->new( $params->{apache} );
-    $self->apache( $apache );
-    $log->is_debug &&
-        $log->debug( "Created Apache::Request object and set in OI request" );
-
-    # Set the URI and parse it
-
-    my $url = $apache->parsed_uri;
-    my $request_path = $url->path;
-    $log->is_debug &&
-        $log->debug( "Got URL from apache '$request_path'" );
-    $self->assign_request_url( $request_path );
-
-    # Setup the GET/SET params
-
-    my $num_params = 0;
-    foreach my $field ( $apache->param() ) {
-        my @values = $apache->param( $field );
-        if ( scalar @values > 1 ) {
-            $self->param( $field, \@values );
-        }
-        else {
-            $self->param( $field, $values[0] );
-        }
-        $num_params++;
-    }
-    $log->is_debug &&
-        $log->debug( "Set all $num_params parameters ok" );
-
-    # Next set the uploaded files
-
-    my $num_uploads = 0;
-    my @upload_names = $apache->upload();
-    $log->is_debug &&
-        $log->debug( "Got upload names: ", join( ", ", @upload_names ) );
-    foreach my $upload_name ( @upload_names ) {
-        $log->is_debug &&
-            $log->debug( "Checking for upload in upload name '$upload_name'..." );
-        my $upload = $apache->upload( $upload_name );
-        $log->is_debug &&
-            $log->debug( "Upload retrieved isa: ", ref $upload );
-        next unless ( ref $upload );
-        my $oi_upload = OpenInteract2::Upload->new(
-            { name         => $upload->name,
-              content_type => $upload->type,
-              size         => $upload->size, # does this work yet??
-              # This might not do what you think... see APR::Brigade (?!)
-              filehandle   => $upload->bb,
-              filename     => $upload->filename,
-              # This doesn't seem to be supported anymore
-              #tmp_name     => $upload->tempname
-         });
-        $self->_set_upload( $upload->name, $oi_upload );
-        $num_uploads++;
-    }
-    $log->is_debug &&
-        $log->debug( "Set all $num_uploads uploaded files" );
-
-    # Then the various headers, properties, etc.
-
-    my $in = $apache->headers_in();
-    $self->referer( $in->{'Referer'} );
-    $self->user_agent( $in->{'User-Agent'} );
-    $self->cookie_header( $in->{'Cookie'} );
-    $self->language_header( $in->{'Accept-Language'} );
-
-    $self->server_name( $apache->get_server_name );
-    $self->remote_host( $apache->connection->remote_addr->ip_get );
-    $log->is_info &&
-        $log->info( "Finished creating Apache 2.x request" );
     return $self;
 }
 
